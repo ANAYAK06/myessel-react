a94b0d28261bd92f067d2ff10266ae60d9867433
Anoop Narayanan <anoop2u@outlook.com>
2025-06-12 15:24:10 +0530

    api changed to esseltestapi for testing cors issue

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'http://esselAPI.esselprojects.com/api'
+  ? 'http://esseltestapi.esselprojects.com/api'
   : 'http://localhost:57771/api';
 
 export { API_BASE_URL };
